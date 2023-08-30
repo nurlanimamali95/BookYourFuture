f29575f473b6a86162650e0b436d588050245966
Anastasia Tikhonova <honu347@gmail.com>
2023-08-30 13:58:56 +0200

    added an icon for emptygroupdata

--- client/src/pages/Admin/AdminDashboard/EventByGroup/EmptyGroupData.js
+++ client/src/pages/Admin/AdminDashboard/EventByGroup/EmptyGroupData.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
+import EventBusyIcon from "@mui/icons-material/EventBusy";
 
 export default function EmptyGroupData() {
   return (
-    <Typography variant="body1">No events scheduled for this group</Typography>
+    <>
+      <EventBusyIcon sx={{ my: 1, fontSize: 40 }} />
+      <Typography variant="body1" sx={{ mb: 2 }}>
+        No events scheduled for this group
+      </Typography>
+    </>
   );
 }
