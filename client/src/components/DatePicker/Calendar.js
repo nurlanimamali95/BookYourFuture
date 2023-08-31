68d0766cb03dba5e83b39514827db3a7c01637db
Nurlan <nurlanimamali@gmail.com>
2023-08-31 17:23:46 +0200

    Responsivness for studentdashbboard

--- client/src/components/DatePicker/Calendar.js
+++ client/src/components/DatePicker/Calendar.js
@@ -3,16 +3,10 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 
-export default function AddWeekNumber() {
+export default function CustomMonthLayout() {
   return (
-    <LocalizationProvider
-      dateAdapter={AdapterDayjs}
-      localeText={{
-        calendarWeekNumberHeaderText: "#",
-        calendarWeekNumberText: (weekNumber) => `${weekNumber}.`,
-      }}
-    >
-      <DateCalendar displayWeekNumber />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar showDaysOutsideCurrentMonth fixedWeekNumber={6} />
     </LocalizationProvider>
   );
 }
