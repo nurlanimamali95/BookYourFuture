3f8df04b380bf2cffa853c59f27fdf0a9c56877b
Anastasia Tikhonova <honu347@gmail.com>
2023-09-01 11:06:34 +0200

    event management table search filters

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,27 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import UserProfilePage from "./pages/userProfilePage/userProfilePage";
-import Layout from "./Layout/Layout";
+// import Layout from "./Layout/Layout";
 import StudentDashboard from "../src/pages/Students/StudentsDashboard";
 import UserList from "./pages/User/UserList";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
+import EventManagement from "./pages/Admin/EventManagementPage";
+import EditEventPage from "./pages/Admin/EditEventPage";
+import AddEventPage from "./pages/Admin/AddEventPage";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/layout" element={<Layout />}>
-          <Route path="student" element={<StudentDashboard />} />
-          <Route path="admin" element={<AdminDashboard />} />
-          <Route path="userList" element={<UserList />} />{" "}
-          <Route path="userProfile" element={<UserProfilePage />} />
-        </Route>
+        {/* <Route path="/layout" element={<Layout />}> */}
+        <Route path="/events" element={<EventManagement />} />
+        <Route path="/edit/:id" element={<EditEventPage />} />
+        <Route path="/add-event" element={<AddEventPage />} />
+        <Route path="student" element={<StudentDashboard />} />
+        <Route path="admin" element={<AdminDashboard />} />
+        <Route path="userList" element={<UserList />} />
+        <Route path="userProfile" element={<UserProfilePage />} />
+        {/* </Route> */}
       </Routes>
     </>
   );
