22593e9920670784815469841bad3fd01431a998
Anastasia Tikhonova <honu347@gmail.com>
2023-09-02 13:22:44 +0200

    add event context

--- client/src/components/Admin/AdminEvents/AddEvent/EventPreview.js
+++ client/src/components/Admin/AdminEvents/AddEvent/EventPreview.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Container, Typography } from "@mui/material";
+import EventContext from "./EventContext";
 
 export default function EventPreview() {
+  const { eventData } = useContext(EventContext);
+
   return (
     <Container>
-      <Typography>Event Preview</Typography>
+      <Typography variant="h6">Event Preview</Typography>
+      <Typography variant="body1">
+        <strong>Title:</strong> {eventData.title}
+      </Typography>
+      <Typography variant="body1">
+        <strong>Description:</strong> {eventData.description}
+      </Typography>
+      <Typography variant="body1">
+        <strong>Duration:</strong> {eventData.duration} minutes
+      </Typography>
+      <Typography variant="body1">
+        <strong>Location:</strong> {eventData.location}
+      </Typography>
+      <Typography variant="body1">
+        <strong>Receiver Type:</strong> {eventData.receiverType}
+      </Typography>
+      <Typography variant="body1">
+        <strong>Group:</strong> {eventData.group}
+      </Typography>
+      <Typography variant="body1">
+        <strong>Student:</strong> {eventData.student}
+      </Typography>
     </Container>
   );
 }
--- client/src/pages/Admin/AddEventPage.js
+++ client/src/pages/Admin/AddEventPage.js
@@ -2,26 +2,38 @@ import React from "react";
 import MainEventInfo from "../../components/Admin/AdminEvents/AddEvent/AddEventInfo";
 import { Grid, Container, Typography, Box } from "@mui/material";
 import EventPreview from "../../components/Admin/AdminEvents/AddEvent/EventPreview";
+import EventContext from "../../components/Admin/AdminEvents/AddEvent/EventContext";
+import { useState } from "react";
 
-const AddEventPage = () => {
+export default function AddEventPage() {
+  const [eventData, setEventData] = useState({
+    duration: "",
+    group: "",
+    student: "",
+    receiverType: "",
+    location: "",
+    title: "",
+    description: "",
+  });
+
   return (
-    <Container>
-      <Typography variant="h3" sx={{ pt: 4, mb: 2, ml: 2 }}>
-        Add Event
-      </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={5} sm={5}>
-          <MainEventInfo />
+    <EventContext.Provider value={{ eventData, setEventData }}>
+      <Container>
+        <Typography variant="h3" sx={{ pt: 4, mb: 2, ml: 2 }}>
+          Add Event
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={5} sm={5}>
+            <MainEventInfo />
+          </Grid>
+          <Grid item xs={2} sm={2}></Grid>
+          <Grid item xs={5} sm={5} sx={{ pl: 5, bgcolor: "lightgray" }}>
+            <Box>
+              <EventPreview />
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={2} sm={2}></Grid>
-        <Grid item xs={5} sm={5} sx={{ pl: 5, bgcolor: "black" }}>
-          <Box>
-            <EventPreview />
-          </Box>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </EventContext.Provider>
   );
-};
-
-export default AddEventPage;
+}
