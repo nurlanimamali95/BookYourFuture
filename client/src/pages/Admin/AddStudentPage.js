52d37aa482b5b2ba9dbc53be616b1cb4830af972
AlevtinaLeytland <clavusclavo321@gmail.com>
2023-09-04 16:09:28 +0200

    add-student page upd

--- client/src/pages/Admin/AddStudentPage.js
+++ client/src/pages/Admin/AddStudentPage.js
@@ -8,6 +8,9 @@ import {
   InputLabel,
   Typography,
   Grid,
+  Container,
+  Stack,
+  Box,
 } from "@mui/material";
 
 const AddStudentPage = () => {
@@ -16,74 +19,105 @@ const AddStudentPage = () => {
   const [email, setEmail] = useState("");
   const [groupNumber, setGroupNumber] = useState("");
 
-  const handleSave = () => {
-    // Handle saving the student data here
-    // console.log("Student data:", { firstName, lastName, email, groupNumber });
+  // const handleSave = () => {
+  //   // Handle saving the student data here
+  //   // eslint-disable-next-line no-console
+  //   // console.log("Student data:", { firstName, lastName, email, groupNumber });
+  // };
+
+  // const handleCancel = () => {
+  //   // Handle cancel action (e.g., navigate back to the student list)
+  // };
+  const buttonStyle = {
+    backgroundColor: "#56ae5a",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "white",
+      color: "#56ae5a",
+      border: "1px solid #56ae5a",
+    },
   };
 
-  const handleCancel = () => {
-    // Handle cancel action (e.g., navigate back to the student list)
+  const cancelButtonStyle = {
+    backgroundColor: "red",
+    color: "white",
+    border: "1px solid red", // Add border style
+    "&:hover": {
+      backgroundColor: "white",
+      color: "red",
+      border: "1px solid red", // Border style on hover
+    },
   };
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Typography variant="h4">Add Student</Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <TextField
-          label="First Name"
-          fullWidth
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-      </Grid>
-      <Grid item xs={6}>
-        <TextField
-          label="Last Name"
-          fullWidth
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <TextField
-          label="Email"
-          fullWidth
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <FormControl fullWidth>
-          <InputLabel>Group Number</InputLabel>
-          <Select
-            value={groupNumber}
-            onChange={(e) => setGroupNumber(e.target.value)}
+    <Container maxWidth="md" sx={{ marginTop: "25px" }}>
+      <Grid container spacing={3}>
+        <Box sx={{ flexGrow: 1 }}>
+          <Typography
+            variant="h4"
+            component="h2"
+            sx={{ mb: 4, mt: 6, textAlign: "center" }}
           >
-            <MenuItem value={43}>43</MenuItem>
-            <MenuItem value={44}>44</MenuItem>
-            <MenuItem value={45}>45</MenuItem>
-            <MenuItem value={46}>46</MenuItem>
-          </Select>
-        </FormControl>
+            Add Student
+          </Typography>
+        </Box>
+        <Grid item xs={12}>
+          <TextField
+            label="First Name"
+            fullWidth
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label="Last Name"
+            fullWidth
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Stack direction="row" spacing={2}>
+            <TextField
+              label="Email"
+              fullWidth
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <FormControl fullWidth>
+              <InputLabel>Group Number</InputLabel>
+              <Select
+                value={groupNumber}
+                onChange={(e) => setGroupNumber(e.target.value)}
+              >
+                <MenuItem value={43}>43</MenuItem>
+                <MenuItem value={44}>44</MenuItem>
+                <MenuItem value={45}>45</MenuItem>
+                <MenuItem value={46}>46</MenuItem>
+              </Select>
+            </FormControl>
+          </Stack>
+        </Grid>
+        <Grid item xs={6} sx={{ justifyContent: "center" }}></Grid>
+        <Grid container justifyContent="center">
+          <Stack
+            direction="row"
+            justifyContent="center"
+            spacing={2}
+            mt={3}
+            sx={{ display: "flex", justifyContent: "center" }}
+          >
+            <Button variant="outlined" sx={cancelButtonStyle}>
+              Cancel
+            </Button>
+            <Button variant="contained" color="primary" sx={buttonStyle}>
+              Save
+            </Button>
+          </Stack>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <Button variant="outlined" fullWidth onClick={handleCancel}>
-          Cancel
-        </Button>
-      </Grid>
-      <Grid item xs={6}>
-        <Button
-          variant="contained"
-          color="primary"
-          fullWidth
-          onClick={handleSave}
-        >
-          Save
-        </Button>
-      </Grid>
-    </Grid>
+    </Container>
   );
 };
 
