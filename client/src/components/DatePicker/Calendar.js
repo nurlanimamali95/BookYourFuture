c11a7321f77a5be3eae5a189dbcf7edaffa2a896
Nurlan <nurlanimamali@gmail.com>
2023-09-14 20:03:38 +0200

    calendar

--- client/src/components/DatePicker/Calendar.js
+++ client/src/components/DatePicker/Calendar.js
@@ -9,6 +9,7 @@ import { DayCalendarSkeleton } from "@mui/x-date-pickers/DayCalendarSkeleton";
 import { todayDate } from "../Student/StudentEventManagement/FormatDate";
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
 
 const initialValue = dayjs(todayDate);
 
@@ -27,28 +28,41 @@ function ServerDay(props) {
 }
 
 export default function Calendar(props) {
+  const userData = useSelector((state) => state.auth.data);
+  const userId = userData ? userData._id : null;
   const { eventsData, onDateSelected } = props;
   const [highlightedDays, setHighlightedDays] = useState([]);
   const [selectedMonth, setSelectedMonth] = useState(initialValue.month());
+  const [selectedYear, setSelectedYear] = useState(initialValue.year());
 
   useEffect(() => {
     const allHighlightedDays = [];
 
     eventsData.forEach((event) => {
       event.sessionSlot.forEach((slot) => {
-        const startTime = dayjs(slot.startTime);
-        allHighlightedDays.push(startTime.date());
+        if (slot.student && slot.student._id === userId) {
+          const startTime = dayjs(slot.startTime);
+          const eventMonth = startTime.month();
+          const eventYear = startTime.year();
+
+          if (eventMonth === selectedMonth && eventYear === selectedYear) {
+            allHighlightedDays.push(startTime.date());
+          }
+        }
       });
     });
 
     setHighlightedDays(allHighlightedDays);
-  }, [eventsData]);
+  }, [eventsData, userId, selectedMonth, selectedYear]);
 
   const handleMonthChange = (newDate) => {
     setSelectedMonth(newDate.month());
+    setSelectedYear(newDate.year());
   };
 
-  const shouldResetHighlightedDays = selectedMonth !== initialValue.month();
+  const shouldResetHighlightedDays =
+    selectedMonth !== initialValue.month() ||
+    selectedYear !== initialValue.year();
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -85,6 +99,9 @@ Calendar.propTypes = {
       sessionSlot: PropTypes.arrayOf(
         PropTypes.shape({
           startTime: PropTypes.string.isRequired,
+          student: PropTypes.shape({
+            _id: PropTypes.string.isRequired,
+          }),
         })
       ).isRequired,
     })
