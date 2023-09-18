486594cb7f64fc231af2ca5ee57ffc9f05ad66d4
EuvhenRight <ugnivenko.ea@gmail.com>
2023-09-18 12:21:14 +0200

    fix problem

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -15,11 +15,15 @@ userRouter.get("/:id", userController.getOne);
 
 userRouter.get("/", checkAuth, userController.userMe);
 
-userRouter.delete("/:id", checkAuth, userController.remove);
+userRouter.delete(
+  "/:id",
+  // checkAuth,
+  userController.remove
+);
 
 userRouter.patch(
   "/edit/:id",
-  checkAuth,
+  // checkAuth,
   editValidationUser,
   validationErrorMessage,
   userController.edit
