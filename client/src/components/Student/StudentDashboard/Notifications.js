c13a8e0c44d64bf067bc0d73783ea3847223c9d9
Nurlan <nurlanimamali@gmail.com>
2023-09-26 13:38:42 +0200

    Button duration fixed

--- client/src/components/Student/StudentDashboard/Notifications.js
+++ client/src/components/Student/StudentDashboard/Notifications.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import Alert from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import "./Notifications.css";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { useRadioContext } from "../StudentEventManagement/TimeSlotContext";
@@ -28,19 +29,20 @@ export default function Notifications(props) {
   return (
     <Stack sx={{ width: "100%" }} spacing={2}>
       {visibleNotifications.map((notification, index) => (
-        <Alert
+        <Link
           key={index}
-          className="animated-alert"
-          action={
-            notification.action ? (
-              <Link
-                to={notification.action.link}
-                onClick={() => {
-                  setEventName(notification.id);
-                  handleDismiss(notification.id);
-                }}
-                sx={{ textDecoration: "none" }}
-              >
+          to={notification.action.link}
+          onClick={() => {
+            setEventName(notification.id);
+            handleDismiss(notification.id);
+          }}
+          style={{ textDecoration: "none" }}
+        >
+          <Alert
+            icon={<InfoOutlinedIcon />}
+            className="animated-alert"
+            action={
+              notification.action ? (
                 <Button
                   style={{ color: "black" }}
                   size="small"
@@ -49,13 +51,13 @@ export default function Notifications(props) {
                   {notification.action.label}
                   <ChevronRightIcon />
                 </Button>
-              </Link>
-            ) : null
-          }
-          severity={notification.type || "info"}
-        >
-          {notification.message}
-        </Alert>
+              ) : null
+            }
+            severity={notification.type || "info"}
+          >
+            {notification.message}
+          </Alert>
+        </Link>
       ))}
     </Stack>
   );
